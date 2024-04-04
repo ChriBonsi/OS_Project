415304ddffc8ccd0e662292eae792792d910da73
Christian Bonsignore <christian.bonsignore@studenti.unicam.it>
2024-04-04 13:51:13 +0200

    Opening, writing and reading in file

--- src/main.c
+++ src/main.c
@@ -4,10 +4,36 @@ int main() {
     FILE *fptr;
 
     // Create a file
-    fptr = fopen("C:/Users/chri9/CLionProjects/SOLAB_Project_Bonsignore/items/newtext.txt", "w");
+    fptr = fopen("../items/newtext.txt", "w");
+    //Scopri perché è segmentation fault
+
+    if (fptr == NULL) {
+        printf("Error opening file!\n");
+        return 1; // Return an error code indicating failure
+    }
+
+    // Write some text to the file
+    fprintf(fptr, "Some text");
 
     // Close the file
     fclose(fptr);
+
+    fptr = fopen("../items/newtext.txt", "r");
+
+    if (fptr == NULL) {
+        printf("Not able to open the file.");
+    } else {
+        // Store the content of the file
+        char myString[100];
+
+        // Read the content and store it inside myString
+        while(fgets(myString, 100, fptr)) {
+            printf("%s", myString);
+        }
+
+        // Close the file
+        fclose(fptr);
+    }
 
     return 0;
 }
